cf6008ecb87c08fe1c57387e6574280ebca63589
rabahallah_yasmine <yasminerabahallah914@gmail.com>
2019-12-15 01:10:32 +0100

    convertir json en csv

--- json-to-csv/src/main/java/fr/uvsq/abdoumassyasmine/App.java
+++ json-to-csv/src/main/java/fr/uvsq/abdoumassyasmine/App.java
@@ -1,13 +1,26 @@
 package fr.uvsq.abdoumassyasmine;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.BasicConfigurator;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	BasicConfigurator.configure();
+    	String chaine="" ;
+   	    /*
+        * Analyser une chaîne JSON et la convertir en CSV
+        */
+   	 List<Map<String, String>>  fluxJson = ReadJson.tarit_Json(chaine);
+        
+   	 fluxJson = ReadJson.parseJson(new File("fichierjson.json"), "UTF-8");
+        Set<String> header =  WriteCsv.collectOrdered( fluxJson);
+        WriteCsv.writeLargeFile( fluxJson, ";", "fichier_csv.csv", header);  
     }
-}
+    }
+
